214a3a23545bc6259ebc967824a3ccc989bf27ba
Anton-Sterky <antonsterky@outlook.com>
2021-08-19 13:24:58 +0200

    played around some more

--- Git-playground/src/Main.java
+++ Git-playground/src/Main.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
 
 public class Main {
+	
+	public Scanner input = new Scanner(System.in);
+	boolean isRunning;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//System.out.println("Hello world!!");
+		Main run;
+		run = new Main();
+		//run.multiplikation();
+		run.gameCycle();
+	}
+	
+	public void multiplikation() {
+		// Räknar ut en multiplikation
+		int x;
+		int y;
+		System.out.println("Vänligen ange 2 siffror");
+		x = input.nextInt();
+		y = input.nextInt();
+		System.out.println(x*y);
+		System.out.println("ya did it dingus");
+	}
+	public void gameCycle() {
+		isRunning = true;
+		int a = 0;
+		while (isRunning) {
+			a = a+1;
+			System.out.println(a);
+			
+			if (a == 100) {
+				isRunning = false;
+			}
+		}
+		System.out.println("klar");
+		
 	}
 
 }
